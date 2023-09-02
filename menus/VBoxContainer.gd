037cc5f8d8935a8c813c229c039fccb5e410ca1d
Dai0w0 <daigamon2@gmail.com>
2023-09-01 20:06:30 -0400

    Implementacion del menu principal
         Se añade el menu principal con titulo Kineko y botones "Jugar", "Config.", "Creditos", "Salir".
         Se implementa el funcionamiento del boton jugar ligandolo con el nivel "test"
         Se implementa el funcionamiento del boton "Salir", al presionarlo se cierra la escena
         Se añade la carpeta "Resources" y en esta se guarda el tema para el menu principal

---
+++ menus/VBoxContainer.gd
@@ -0,0 +1,19 @@
+extends VBoxContainer
+
+
+@onready var jugar = %Jugar
+@onready var config = %Config
+@onready var creditos = %Creditos
+@onready var salir = %Salir
+
+func _ready() -> void:
+	jugar.pressed.connect(_on_jugar_pressed)
+	#config.pressed.connect(_on_config_pressed)
+	#creditos.pressed.connect(_on_creditos_pressed)
+	salir.pressed.connect(_on_salir_pressed)
+	
+func _on_jugar_pressed():
+	get_tree().change_scene_to_file("res://levels/test/knight_moves.tscn")
+func _on_salir_pressed():
+	get_tree().quit()
+
