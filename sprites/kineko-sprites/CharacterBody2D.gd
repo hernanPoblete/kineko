369c68582ca3ce16935fcc34bc4ada0727f4340c
hernanPoblete <hernan.poblete@ug.uchile.cl>
2023-09-25 11:26:11 -0300

    gato añadido

---
+++ sprites/kineko-sprites/CharacterBody2D.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+
+const SPEED = 1000.0
+const colors = [Color(0.95686, 0.81176, 0.70588), Color(0,1,0), Color(0,0,1)]
+var color_selected = -1
+var rest = 0
+
+func _physics_process(delta: float)-> void:
+	var direction_x = Input.get_axis("move_left", "move_right")
+	var direction_y = Input.get_axis("move_up", "move_down")	
+	
+
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if direction_x and direction_y:
+		velocity.x = direction_x*SPEED/1.41
+		velocity.y = direction_y*SPEED/1.41
+	if rest !=0:
+		rest = rest-1
+
+	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if rest == 0:
+		if event.is_action_pressed("color_left"):
+			#El +len(colors previene que en 0 el indice cambie a -1)
+			color_selected = (color_selected-1+len(colors))%len(colors)
+		elif event.is_action_pressed("color_right"):
+			color_selected = (color_selected+1)%len(colors)
+		
+		if event.is_action_pressed("color_left") or event.is_action_pressed("color_right"):
+			$"Gorritowo".modulate = colors[color_selected]
+			rest = rest + 60
