7bf6cec098355ef52f4092cd9eadb3186cc2eec4
hernanPoblete <hernan.poblete@ug.uchile.cl>
2023-10-01 19:18:58 -0300

    se crea menu de seleccion de colores. Falta agregar guardado de archivos

--- menus/VBoxContainer.gd
+++ menus/VBoxContainer.gd
@@ -8,12 +8,14 @@ extends VBoxContainer
 
 func _ready() -> void:
 	jugar.pressed.connect(_on_jugar_pressed)
-	#config.pressed.connect(_on_config_pressed)
+	config.pressed.connect(_on_config_pressed)
 	#creditos.pressed.connect(_on_creditos_pressed)
 	salir.pressed.connect(_on_salir_pressed)
 	
 func _on_jugar_pressed():
 	get_tree().change_scene_to_file("res://levels/test/knight_moves.tscn")
+func _on_config_pressed():
+	get_tree().change_scene_to_file("res://menus/colors.tscn")
 func _on_salir_pressed():
 	get_tree().quit()
 
