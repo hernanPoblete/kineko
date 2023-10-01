46169e5da9e4b5022157e2159d69d9cad0bcafdd
Dai0w0 <daigamon2@gmail.com>
2023-10-01 20:21:36 -0300

    Implementacion del menu de pausa
    Nodo "Pause_menu" añadido junto con su script y asociado como nodo heredado a "Knight", cambie el estado de los nodos "Pause_menu" y "Main_menu" para que esten corriendo constantemente y asi son interactuables durante la pausa

---
+++ menus/Pause_Menu.gd
@@ -0,0 +1,31 @@
+extends VBoxContainer
+
+@onready var continuar = %Continuar
+@onready var config = %Config
+@onready var menu_principal = %MenuPrincipal
+@onready var salir = %Salir
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	continuar.pressed.connect(_on_continuar_pressed)
+	#config.pressed.connect(_on_config_pressed)
+	menu_principal.pressed.connect(_on_menu_principal_pressed)
+	salir.pressed.connect(_on_salir_pressed)
+	hide()
+	
+func _input(event: InputEvent)-> void:
+	if event.is_action_pressed("Pause"):
+		visible = !visible
+		get_tree().paused = visible
+		
+func _on_continuar_pressed():
+	hide()
+func _on_menu_principal_pressed():
+	get_tree().change_scene_to_file("res://menus/Main_Menu.tscn")
+	
+func _on_salir_pressed():
+	get_tree().quit()
+
+
+
