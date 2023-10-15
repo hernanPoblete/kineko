d0299db381268e1d76ce17714fe851472e0a0e41
Nabil Orellana <nabil.patagonia@gmail.com>
2023-10-15 16:58:43 -0300

    muros con placeholder uwu

--- sprites/kineko-sprites/CharacterBody2D.gd
+++ sprites/kineko-sprites/CharacterBody2D.gd
@@ -1,3 +1,4 @@
+class_name kineko
 extends CharacterBody2D
 
 
@@ -5,6 +6,9 @@ const SPEED = 1000.0
 const colors = [Color(0.95686, 0.81176, 0.70588), Color(0,1,0), Color(0,0,1)]
 var color_selected = -1
 var rest = 0
+
+func _ready():
+	ColorGlobal.ColorGlobal = color_selected
 
 func _physics_process(delta: float)-> void:
 	var direction_x = Input.get_axis("move_left", "move_right")
@@ -34,9 +38,13 @@ func _input(event: InputEvent) -> void:
 		if event.is_action_pressed("color_left"):
 			#El +len(colors previene que en 0 el indice cambie a -1)
 			color_selected = (color_selected-1+len(colors))%len(colors)
+			ColorGlobal.ColorGlobal = color_selected
 		elif event.is_action_pressed("color_right"):
 			color_selected = (color_selected+1)%len(colors)
+			ColorGlobal.ColorGlobal = color_selected
 		
 		if event.is_action_pressed("color_left") or event.is_action_pressed("color_right"):
 			$"Gorritowo".modulate = colors[color_selected]
 			rest = rest + 60
+func kill():
+	position = Vector2(1050,40)
