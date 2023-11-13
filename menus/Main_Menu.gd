fdc532c7a150247a2c86233389012c9c43a2621f
hernanPoblete <hernan.poblete@ug.uchile.cl>
2023-11-13 11:41:33 -0300

    Color de fondo añadido. Tocará cambiarlo en algún momento

--- menus/Main_Menu.gd
+++ menus/Main_Menu.gd
@@ -8,15 +8,24 @@ extends VBoxContainer
 
 #Checkea si existe un archivo con los colores. Crea uno default si no existe
 func check_colors()->void:
-	var c = FileAccess.open("user://color_data.json", FileAccess.WRITE_READ)
-	if not c.get_as_text():
-		var color_default = [[1,0,0],[0,1,0],[0,0,1]]
-		c.store_string(JSON.stringify(color_default))
+	var color
+	if not FileAccess.file_exists("user://color_data.json"):
+		var default_colors = [[1,0,0],[0,1,0],[0,0,1]]
+		var f = FileAccess.open("user://color_data.json", FileAccess.WRITE)
+		f.store_string(JSON.stringify(default_colors))
+		f.close()
+		color = default_colors.map(func(x): return Color(x[0], x[1], x[2]))
+	else:
+		var f = FileAccess.open("user://color_data.json", FileAccess.READ)
+		var data = JSON.parse_string(f.get_as_text())
+
+		color = data.map(func(x): return Color(x[0], x[1], x[2]))
+		RenderingServer.set_default_clear_color((color[0]+color[1]+color[2]).inverted()/3)
 
 func _ready() -> void:
 	check_colors()
 	jugar.pressed.connect(_on_jugar_pressed)
-	#config.pressed.connect(_on_config_pressed)
+	config.pressed.connect(_on_config_pressed)
 	#creditos.pressed.connect(_on_creditos_pressed)
 	salir.pressed.connect(_on_salir_pressed)
 	
@@ -24,4 +33,6 @@ func _on_jugar_pressed():
 	get_tree().change_scene_to_file("res://levels/test/knight_moves.tscn")
 func _on_salir_pressed():
 	get_tree().quit()
+func _on_config_pressed():
+	get_tree().change_scene_to_file("res://menus/colors.tscn")
 
--- menus/color_menu.gd
+++ menus/color_menu.gd
@@ -1,23 +1,25 @@
 extends Control
 
-#Poner en color la variable global de los colores (branch de Nabil)
-@export var color: Array[Color]=[Color(0,0,1), Color(0,1,0), Color(1,0,0)]
-@export var new: Array[Color]=color
+var color_data = FileAccess.open("user://color_data.json", FileAccess.READ)
+var color = JSON.parse_string(color_data.get_as_text()).map(
+	func(x):return Color(x[0], x[1], x[2])
+)
 
 
 @onready var si = $HBoxContainer/si
 @onready var no = $HBoxContainer/no
 @onready var res = $HBoxContainer/res
 
+
 func _ready():
-	$"preview/0".color = new[0]
-	$"preview/1".color = new[1]
-	$"preview/2".color = new[2]
+	color_data.close()
+	$"preview/0".color = color[0]
+	$"preview/1".color = color[1]
+	$"preview/2".color = color[2]
 	
 	$"pickers/Color 0".color = color[0]
 	$"pickers/Color 1".color = color[1]
 	$"pickers/Color 2".color = color[2]
-	
 	
 	si.pressed.connect(guardar_colores)
 	no.pressed.connect(menu_principal)
@@ -26,18 +28,29 @@ func _ready():
 
 
 func _process(delta):
-	new[0]=$"pickers/Color 0".color
-	new[1]=$"pickers/Color 1".color
-	new[2]=$"pickers/Color 2".color
-	$"preview/0".color = new[0]
-	$"preview/1".color = new[1]
-	$"preview/2".color = new[2]
+	color[0]=$"pickers/Color 0".color
+	color[1]=$"pickers/Color 1".color
+	color[2]=$"pickers/Color 2".color
+	$"preview/0".color = color[0]
+	$"preview/1".color = color[1]
+	$"preview/2".color = color[2]
+	
+	RenderingServer.set_default_clear_color((color[0]+color[1]+color[2]).inverted()/3)
 
 func reset_colores():
-	get_tree().change_scene_to_file("res://menus/colors.tscn")
+	$"pickers/Color 0".color = Color(0,0,1)
+	$"pickers/Color 1".color = Color(0,1,0)
+	$"pickers/Color 2".color = Color(1,0,0)
 func guardar_colores():
-	print("uwu")
+	var new_Colors = [
+		[$"pickers/Color 0".color[0], $"pickers/Color 0".color[1], $"pickers/Color 0".color[2]],
+		[$"pickers/Color 1".color[0], $"pickers/Color 1".color[1], $"pickers/Color 1".color[2]],
+		[$"pickers/Color 2".color[0], $"pickers/Color 2".color[1], $"pickers/Color 2".color[2]],
+	]
+	
+	var color_data = FileAccess.open("user://color_data.json", FileAccess.WRITE)
+	color_data.store_string(JSON.stringify(new_Colors))
 func menu_principal():
-	get_tree().change_scene_to_file("res://menus/inicio.tscn")
+	get_tree().change_scene_to_file("res://menus/Main_Menu.tscn")
 
 
