414e0f041458ea1c466d3a75c23cdcdd942e7e8e
Nabil Orellana <nabil.patagonia@gmail.com>
2023-12-02 17:05:50 -0300

    funcion kill y animaciones

--- sprites/kineko-sprites/kineko.gd
+++ sprites/kineko-sprites/kineko.gd
@@ -2,12 +2,12 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const push_speed = 500
 var color_file = JSON.parse_string(FileAccess.open("user://color_data.json", FileAccess.READ).get_as_text())
 var colors = color_file.map(func(x): return Color(x[0], x[1], x[2]))
 var color_selected = -1
 var rest = 0
 @export var dead = false
+@export var level_position = Vector2()
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
 var sleep = 300
@@ -15,16 +15,19 @@ var sleep = 300
 func _ready():
 	animation_tree.active = true
 	playback.travel("run")
+	ColorGlobal.Colores = colors
 	
 func _physics_process(delta: float)-> void:
 	var direction_x = Input.get_axis("move_left", "move_right")
 	var direction_y = Input.get_axis("move_up", "move_down")
 	
+	
 	if (direction_x or direction_y):
 		velocity.x = direction_x * SPEED/sqrt(direction_x**2 + direction_y**2)
 		velocity.y = direction_y * SPEED/sqrt(direction_x**2 + direction_y**2)
-		playback.travel("run")
 		sleep = 300
+		if dead == false:
+			playback.travel("run")
 	else:
 		velocity.x = 0
 		velocity.y = 0
@@ -34,11 +37,21 @@ func _physics_process(delta: float)-> void:
 		else:
 			playback.travel("idle")
 	
-	
-	
 	if rest !=0:
 		rest = rest-1
 	move_and_slide()
+
+func kill():
+	dead = true
+	playback.travel("dead")
+
+func teleport():
+	if dead == true:
+		position = level_position
+		dead = false
+
+func win():
+	print("you win :D")
 
 func _input(event: InputEvent) -> void:
 	if rest == 0:
